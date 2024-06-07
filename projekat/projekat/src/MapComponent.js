d8a6d662b68854329d71661ad3aef92ebaf0b619
Danilo Congradac <48456090+danilocongradac@users.noreply.github.com>
2024-06-07 14:04:17 +0200

    Selekcija Vise Objekata
    Radi kako treba layer switcher, selekcija objekata i racunjae njihove povrsine

--- projekat/projekat/src/MapComponent.js
+++ projekat/projekat/src/MapComponent.js
@@ -10,11 +10,16 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import {bbox as bboxStrategy} from 'ol/loadingstrategy';
 import GeoJSON from 'ol/format/GeoJSON';
+
+import Select from 'ol/interaction/Select';
+import {altKeyOnly, click, pointerMove, platformModifierKey, platformModifierKeyOnly} from 'ol/events/condition';
+
 import './map.css'
 import MeniComponent from './MeniComponent';
 
 const MapComponent = () => {
   let layers = []
+  
 
   const toggleLandUse = (event) => {
     layers[1].setVisible(event.target.checked);
@@ -24,12 +29,14 @@ const MapComponent = () => {
     layers[2].setVisible(event.target.checked);
   }
 
-  const setAllLayersHidden = () => {
-    layers.array.forEach(element => {
-        if (layers.getIndex(element) != 0){
-          element.setVisible(false)
-        }
-    });
+  const toggleObjekti = (event) => {
+    layers[3].setVisible(event.target.checked);
+  }
+
+  const setAllLayersHidden = () => {    
+    for(let i=1; i < layers.length; i++){
+      layers[i].setVisible(false)
+    }
   }
 
   const raster = new TileLayer({
@@ -49,10 +56,47 @@ const MapComponent = () => {
         format: new GeoJSON()
       }),
   });
- 
+  
+  const objekti = new VectorLayer({
+    source: new VectorSource({
+      url: '/PODACI/objekti_ns.geojson',
+        format: new GeoJSON()
+      }),
+  });
+  
+  layers = [raster, landuse, roads, objekti]
+
+  setAllLayersHidden();
+
+  //stil za prikaz selekcije
+  const selectStyleSingle = new Style({
+    fill: new Fill({
+      color: 'rgba(0, 0, 255, 0.7)',
+    }),
+    stroke: new Stroke({
+      color: 'black',
+      width: 2,
+    }),
+  });
+  const selectStyleMultiple = new Style({
+    fill: new Fill({
+      color: 'rgba(255, 0, 0, 0.7)',
+    }),
+    stroke: new Stroke({
+      color: 'black',
+      width: 2,
+    }),
+  });
+
+  const selected = [];
+
+  const selectClickMultiple = new Select({
+    condition: click ,
+  });
+  
+  const [ukupnaPovrsina, setUkupnaPovrsina] = useState(0);
+
   useEffect( () => {
-    layers = [raster, landuse, roads]
-        
     const map = new Map({
       target: 'map',
       layers: layers,
@@ -61,16 +105,42 @@ const MapComponent = () => {
         zoom: 14,
       })
     });
+    
+
+    if (click){
+      console.log('click');
+    }
+
+    map.on('singleclick' , function (e) {
+      map.forEachFeatureAtPixel(e.pixel, function (f) {
+        const selIndex = selected.indexOf(f);
+        let povrsina=Math.round(f.getGeometry().getArea());
+        if (selIndex < 0) {
+          selected.push(f);
+          f.setStyle(selectStyleMultiple);
+          setUkupnaPovrsina(sUkupnaPovrsina => sUkupnaPovrsina + povrsina);
+        } else {
+          selected.splice(selIndex, 1);
+          f.setStyle(undefined);
+          setUkupnaPovrsina(sUkupnaPovrsina => sUkupnaPovrsina - povrsina);
+        }
+
+        //highlightStyle.getText().setText('P = ' + ukupnaPovrsina + ' m2');
+      });
+    
+      console.log('ukupna povrsina: ', ukupnaPovrsina, 'm2');
+    });
 
     return () => {
       map.setTarget(null);
     };
-
   }, []);  
+  
   return (
     <div id='glavni'>
       <div id="map"></div>
-      <MeniComponent check1={toggleLandUse} check2={toggleRoads}/>
+      <MeniComponent check1={toggleLandUse} check2={toggleRoads} check3={toggleObjekti}/>
+      <p id="ukupnaPovrsina">Ukupna povrsina selektovanih objekata: {ukupnaPovrsina}</p>
     </div>
   )
 };
--- projekat/projekat/src/MeniComponent.js
+++ projekat/projekat/src/MeniComponent.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useRef} from 'react'
 
-const MeniComponent = ( {check1, check2} ) => {
+const MeniComponent = ( {check1, check2, check3} ) => {
    return (
         <div id="menu">
         <input type='checkbox' id='btn1' onChange={check1}></input>
         <input type='checkbox' id='btn2' onChange={check2}></input>
+        <input type='checkbox' id='btn3' onChange={check3}></input>
         </div>    
     )
   };
